d01a49465da25ceea317736f6e4597c092c72bf1
Dede <dede@cakravala.id>
2023-05-14 20:59:12 +0700

    check point

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -5,6 +5,7 @@ import propTypes from "prop-types";
 export default function Button(props) {
   const className = [props.className];
   if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
   if (props.isLarge) className.push("btn-lg");
   if (props.isSmall) className.push("btn-sm");
   if (props.isBlock) className.push("btn-block");
@@ -15,7 +16,7 @@ export default function Button(props) {
   };
 
   if (props.isDisable || props.isLoading) {
-    if (props.isDisable) className.push("disabled");
+    if (props.isDisabled) className.push("disabled");
     return (
       <span className={className.join(" ")} style={props.style}>
         {props.isLoading ? (
@@ -69,11 +70,14 @@ export default function Button(props) {
 }
 
 Button.propTypes = {
-  type: propTypes.oneOf(["button", "Links"]),
-  onclick: propTypes.func,
+  type: propTypes.oneOf(["button", "link"]),
+  onClick: propTypes.func,
+  href: propTypes.string,
   target: propTypes.string,
   className: propTypes.string,
-  isDisable: propTypes.bool,
+  isPrimary: propTypes.bool,
+  isLight: propTypes.bool,
+  isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
