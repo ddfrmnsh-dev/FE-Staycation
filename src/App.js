5d9640a9d312c7954585571f97258eec1cc0a8db
Dede <dede@cakravala.id>
2023-05-17 14:48:42 +0700

    check point hero

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import BookingPage from "pages/BookingPage";
 
 function App() {
   return (
